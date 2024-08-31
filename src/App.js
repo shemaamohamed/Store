f9cd489e9269cc0549f0e5016c8321b74a508f41
MahmoudAliEid <alim46972@gmail.com>
2024-09-01 00:45:54 +0300

    fix some errors

--- src/App.js
+++ src/App.js
@@ -1,49 +1,102 @@
-//دلوقتي انا اعتمدت علي فكرة class 
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import Nav from "./components/nav";
+import Content from "./components/content";
+import AddCard from "./components/Addcard";
+import SignUp from "./components/Signup";
+import Login from "./components/Login";
 
-import { Component  ,useEffect ,useState } from 'react';
-import {BrowserRouter ,Route ,Routes ,Navigate} from 'react-router-dom'
-import Nav from './components/nav';
-import Content from './components/content';
-import AddCard from './components/Addcard';
-import Card from './components/card';
-import Cards from './components/cards';
-import SignUp from './components/Signup';
-import Login from './components/Login';
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
 
-function App ( ) {
+function AppContent() {
   const [isLogin, setIsLogin] = useState(false);
-  localStorage.setItem('islogin', 'false');
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Get the current path
+  const currentPath = location.pathname;
 
   useEffect(() => {
-    // Simulate fetching login status, e.g., from localStorage
-    const loginStatus = localStorage.getItem('islogin') === 'true';
-    setIsLogin(loginStatus);
+    // want this to run every time the component is rendered or re-rendered or change happen in local storage
+    const triggerChange = setInterval(() => {
+      const isLoginTrigger = localStorage.getItem("isLogin") ? true : false;
+      setIsLogin(isLoginTrigger);
+    }, 1000);
+    // cleanup function clear the interval
+    return () => {
+      // clear the interval for performance and memory and avoid memory leak
+      clearInterval(triggerChange);
+    };
   }, []);
- 
-  const PrivateRoute = ({ element }) => {
-    const islogin=localStorage.getItem('islogin')
-    return islogin=='true'? element : <Navigate to="/login" />;
+
+  // useEffect(() => {
+
+  //   const myName ="Shimaa" ;
+  //   alert("You are logged in");
+  //   console.log("You are logged in", myName);
+
+  // }, []); // work one time when the component is mounted
+
+  // check if user is logged in to prevent access to certain routes /login /signup
+  useEffect(() => {
+    if (currentPath === "/login" || currentPath === "/signup") {
+      // and user is logged in isLogin = true
+      if (isLogin) {
+        navigate("/content");
+      }
+    }
+  }, [currentPath, isLogin, navigate]);
+
+  const PrivateRoute = ({ children }) => {
+    const isLogin = localStorage.getItem("isLogin");
+
+    // ** first time isLogin = null or false
+    // ** return to login page
+
+    // ** if user logged in isLogin = true
+    // ** return to content page
+    return isLogin ? children : <Navigate to="/login" />;
   };
 
-
-  
-    return (
-      <>
-      <BrowserRouter>
-      
-          <Nav islogin={isLogin} />
-          <Routes>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login  />} />
-            <Route path="/content" element={<PrivateRoute element={<Content />} />} />
-            <Route path="/add" element={<PrivateRoute element={<AddCard />} />} />   
-          </Routes>
-      </BrowserRouter>
-      </>
-      
-    );
-
-  
+  return (
+    <>
+      <Nav isLogin={isLogin} />
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/content"
+          element={
+            <PrivateRoute>
+              <Content />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add"
+          element={
+            <PrivateRoute>
+              <AddCard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,41 +1,31 @@
-import { Component, useEffect, useRef, useState } from 'react';
-import '../Style/nav.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faMagnifyingGlass , faUser , faHeart ,faCartShopping} from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
+import "../Style/nav.css";
+import { Link } from "react-router-dom";
 
-function Nav (probs) {
-  const {isLogin }=probs;
-  console.log(isLogin)
-  
+function Nav(props) {
+  const { isLogin } = props;
 
-    return (
-      <ul className='list'>
-        <li className='category'>
-        <li><Link    to='/content'>All Products</Link></li>
-        <li><Link to='/add'>Add Products</Link></li>
-        {!isLogin && (
-        <>
-          <li className='category'>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li className='category'>
-            <Link to='/signup'>Signup</Link>
-          </li>
-        </>
-      )}
+  return (
+    <ul className="list">
+      <li className="category">
+        <li>
+          <Link to="/content">All Products</Link>
         </li>
-        
-
-        
-        
-        
-
-
-      </ul>
-    );
-
-  }
-  
+        <li>
+          <Link to="/add">Add Products</Link>
+        </li>
+        {!isLogin && (
+          <>
+            <li className="category">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="category">
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+      </li>
+    </ul>
+  );
+}
 
 export default Nav;
